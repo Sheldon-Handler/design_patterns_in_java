f284f2a1d8b9cb8d4b5c6b18c10dd809b9ca5459
Sheldon_Handler <57599804+Sheldon-Handler@users.noreply.github.com>
2023-09-01 13:47:27 -0400

    Middle of working on it

---
+++ src/main/java/com/activemesa/structural/decorator/ad/AdapterDecorator.java
@@ -0,0 +1,21 @@
+package com.activemesa.structural.decorator.ad;
+
+import java.util.stream.IntStream;
+public class AdapterDecorator {
+}
+
+class MyStringBuilder {
+    private StringBuilder sb = new StringBuilder();
+
+    public MyStringBuilder(StringBuilder sb) {
+        sb = new StringBuilder();
+    }
+
+
+}
+
+class AdapterDecoratorDemo {
+    public static void main(String[] args) {
+
+    }
+}
