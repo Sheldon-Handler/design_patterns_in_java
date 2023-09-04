57af56169be11f0afe0950368fb9ee6a5c85c6c9
Sheldon_Handler <57599804+Sheldon-Handler@users.noreply.github.com>
2023-09-04 16:32:34 -0400

    Section 10: Decorator completed

---
+++ src/main/java/com/activemesa/structural/decorator/exercise/DecoratorExercise.java
@@ -0,0 +1,45 @@
+package com.activemesa.structural.decorator.exercise;
+
+class Bird
+{
+    public int age;
+
+    public String fly()
+    {
+        return age < 10 ? "flying" : "too old";
+    }
+}
+
+class Lizard
+{
+    public int age;
+
+    public String crawl()
+    {
+        return (age > 1) ? "crawling" : "too young";
+    }
+}
+
+class Dragon
+{
+    private int age;
+    private Bird bird = new Bird();
+    private Lizard lizard = new Lizard();
+    public void setAge(int age)
+    {
+        // todo
+        this.age = bird.age = lizard.age = age;
+    }
+
+    public String fly()
+    {
+        // todo
+        return bird.fly();
+
+    }
+    public String crawl()
+    {
+        // todo
+        return lizard.crawl();
+    }
+}
