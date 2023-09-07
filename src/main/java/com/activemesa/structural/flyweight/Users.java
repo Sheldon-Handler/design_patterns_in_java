5978f1beb129daa5be10f58bb775e710419028d5
Sheldon_Handler <57599804+Sheldon-Handler@users.noreply.github.com>
2023-09-07 16:03:47 -0400

    Completed Flyweight section

---
+++ src/main/java/com/activemesa/structural/flyweight/Users.java
@@ -0,0 +1,41 @@
+package com.activemesa.structural.flyweight;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+class User {
+    private String fullName;
+
+    public User(String fullName) {
+        this.fullName = fullName;
+    }
+}
+
+class User2 {
+    static List<String> strings = new ArrayList<>();
+    private int[] names;
+
+    public User2(String fullName) {
+        Function<String, Integer> getOrAdd = (String s) -> {
+            int idx = strings.indexOf(s);
+            if (idx != -1) return idx;
+            else {
+                strings.add(s);
+                return strings.size()-1;
+            }
+        };
+
+        names = Arrays.stream(fullName.split(" "))
+                .mapToInt(s -> getOrAdd.apply(s))
+                .toArray();
+    }
+}
+
+class UsersDemo {
+    public static void main(String[] args) {
+        User2 user = new User2("John Smith");
+        User2 user2 = new User2("Jane Smith");
+    }
+}
---
+++ src/main/java/com/activemesa/structural/flyweight/exercise/FlyweightExercise.java
@@ -0,0 +1,40 @@
+package com.activemesa.structural.flyweight.exercise;
+
+import java.util.*;
+
+class Sentence
+{
+    private String[] words;
+    private Map<Integer, WordToken> tokens = new HashMap<>();
+
+    public Sentence(String plainText)
+    {
+        words = plainText.split(" ");
+    }
+
+    public WordToken getWord(int index)
+    {
+        WordToken wordToken = new WordToken();
+        tokens.put(index, wordToken);
+        return tokens.get(index);
+    }
+
+    @Override
+    public String toString()
+    {
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        for (int i = 0; i < words.length; ++i)
+        {
+            String word = words[i];
+            if (tokens.containsKey(i) && tokens.get(i).capitalize)
+                word = word.toUpperCase();
+            stringArrayList.add(word);
+        }
+        return String.join(" ", stringArrayList);
+    }
+
+    class WordToken
+    {
+        public boolean capitalize;
+    }
+}
